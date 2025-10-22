6e9f1b18457b77079464b7722a4e4a6b738c680f
Aaron Neugebauer <72529530+PantomInach@users.noreply.github.com>
2025-10-22 19:08:34 +0200

    Fixed issues setting Status for LpSolution when using Gurobi and WithMaxSeconds (#12)
    * fix: Wrongly setting instance as infeasible when terminated by time
    limit
    
    * feat: introduce more status options
    
    ---------
    
    Co-authored-by: Aaron Neugebauer <aaron.neugebauer@stud-mail.uni-wuerzburg.de>

--- src/solvers/gurobi.rs
+++ src/solvers/gurobi.rs
@@ -156,12 +156,18 @@ impl SolverProgram for GurobiSolver {
     }
 
     fn parse_stdout_status(&self, stdout: &[u8]) -> Option<Status> {
-        if buf_contains(stdout, "Optimal solution found") {
+        if buf_contains(stdout, "Optimal solution found (tolerance 1.00e-04)") {
             Some(Status::Optimal)
-        } else if buf_contains(stdout, "infeasible") {
-            Some(Status::Infeasible)
+        } else if buf_contains(stdout, "Optimal solution found (tolerance ") {
+            Some(Status::MipGap)
         } else if buf_contains(stdout, "Time limit reached") {
-            Some(Status::SubOptimal)
+            if buf_contains(stdout, "Best objective -,") {
+                Some(Status::NotSolved)
+            } else {
+                Some(Status::TimeLimit)
+            }
+        } else if buf_contains(stdout, "infeasible") || buf_contains(stdout, "Infeasible model") {
+            Some(Status::Infeasible)
         } else {
             None
         }
